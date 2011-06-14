88bee6aaa5c9c7dfdd40b6340bac0bf887839e5b
Rob McBroom <github@skurfer.com>
2011-06-14 11:12:43 -0400

    make recommended colors persist across restarts

--- QSNostromoPrefButtons.m
+++ QSNostromoPrefButtons.m
@@ -12,7 +12,12 @@
 - (IBAction)setRecommendedColors:(id)sender
 {
     // load colors from a file and set them in Quicksilver's preferences
-    [[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithContentsOfFile:[[[NSBundle bundleWithIdentifier:@"com.qsapp.Quicksilver.NostromoInterface"] bundlePath] stringByAppendingPathComponent:@"Contents/Resources/QSNostromoColors.plist"]]];
+    NSDictionary *nostromoColors = [NSDictionary dictionaryWithContentsOfFile:[[[NSBundle bundleWithIdentifier:@"com.qsapp.Quicksilver.NostromoInterface"] bundlePath] stringByAppendingPathComponent:@"Contents/Resources/QSNostromoColors.plist"]];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    for (NSString *colorKey in [nostromoColors allKeys]) {
+        [defaults setObject:[nostromoColors objectForKey:colorKey] forKey:colorKey];
+    }
+    [defaults synchronize];
 }
 
 @end
