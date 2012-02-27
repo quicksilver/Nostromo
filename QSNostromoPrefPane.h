dcf102f43d98807057a26b2a0912e22489873a73
Rob McBroom <github@skurfer.com>
2012-02-27 11:37:07 -0500

    add a class for the preference pane

---
+++ QSNostromoPrefPane.h
@@ -0,0 +1,13 @@
+//
+//  QSNostromoPrefPane.h
+//  Nostromo
+//
+//  Created by Rob McBroom on 2012/02/27.
+//  Copyright (c) 2012 Skurfers’ Alliance. All rights reserved.
+//
+
+#import <QSInterface/QSInterface.h>
+
+@interface QSNostromoPrefPane : QSPreferencePane
+
+@end
---
+++ QSNostromoPrefPane.m
@@ -0,0 +1,13 @@
+//
+//  QSNostromoPrefPane.m
+//  Nostromo
+//
+//  Created by Rob McBroom on 2012/02/27.
+//  Copyright (c) 2012 Skurfers’ Alliance. All rights reserved.
+//
+
+#import "QSNostromoPrefPane.h"
+
+@implementation QSNostromoPrefPane
+
+@end
