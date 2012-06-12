47ef0f1e6cd5333232992c2b0e2da6464e1a9f93
Rob McBroom <github@skurfer.com>
2012-06-12 18:30:52 -0400

    remove redundant include statements

--- QSNostromoInterfaceController.h
+++ QSNostromoInterfaceController.h
@@ -5,9 +5,6 @@
 //  Created by Eric Doughty-Papassideris on 29/04/2011.
 //
 
-#import <Cocoa/Cocoa.h>
-#import <QSFoundation/QSFoundation.h>
-#import <QSCore/QSCore.h>
 #import <QSInterface/QSResizingInterfaceController.h>
 
 @interface QSNostromoInterfaceController : QSResizingInterfaceController {
