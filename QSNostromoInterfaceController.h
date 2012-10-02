64546fcde9ff92cbe6a300ffc3841bb3c7f430f5
Rob McBroom <github@skurfer.com>
2012-10-02 09:07:35 -0400

    remove unused files and import statements

--- QSNostromoInterfaceController.h
+++ QSNostromoInterfaceController.h
@@ -5,8 +5,6 @@
 //  Created by Eric Doughty-Papassideris on 29/04/2011.
 //
 
-#import <QSInterface/QSResizingInterfaceController.h>
-
 @interface QSNostromoInterfaceController : QSResizingInterfaceController {
     NSRect standardRect;
     NSRect standardIObjectRect;
--- QSNostromoPrefPane.h
+++ QSNostromoPrefPane.h
@@ -5,8 +5,6 @@
 //  Created by Rob McBroom on 2012/02/27.
 //
 
-#import <QSInterface/QSInterface.h>
-
 @interface QSNostromoPrefPane : QSPreferencePane
 {
 	NSArray *themes;
